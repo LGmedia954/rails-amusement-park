c25b6e5ee02c644a97a07294ce918f0f706a090e
LGmedia954 <LeslieGonzalez@me.com>
2021-08-03 22:07:25 -0400

    11 tests pending

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    #returns 'sad' when the user is more nauseous than happy
+    unless admin
+      happiness > nausea ? "happy" : "sad"
+    end
+  end
+
 end
